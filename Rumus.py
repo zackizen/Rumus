531eeab0b5a29aae040a640357d54a358eda983c
zackizen <150909591+zackizen@users.noreply.github.com>
2023-12-13 16:08:13 +0700

    Rumus

---
+++ Rumus.py
@@ -0,0 +1,15 @@
+print("----------Program rumus-------------")
+
+lebar = int(input("Masukan nilai lebar: "))
+panjang = int(input("Masukan nilai panjang: " ))
+# untuk mencari nilai luas
+luas = panjang*lebar
+
+# untuk mencari keliling
+keliling = 2*(lebar + panjang)
+
+# Untuk nilai luas
+print("Masukan nilai Luas :",luas)
+
+# untuk nilai keliling
+print("Masukan nilai keliling :",keliling)
